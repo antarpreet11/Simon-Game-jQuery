ae6e0784a8f363e14bc09d3ef013a25fcd84c76a
antarpreet11 <singhantarpreet11@gmail.com>
2022-09-29 22:56:21 -0400

    finished all logic - levels, game over and restart

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
 let gamePattern = []
 let userClickedPattern = []
+let started = false;
+let level = 0;
 
 const buttonColors = ["red", "blue", "green", "yellow"]
 
 const nextSequence = () => {
-    // let randomNumber = 
+    userClickedPattern = []
     let randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
     gamePattern.push(randomChosenColor)
-
     $("#"+randomChosenColor).fadeOut(80).fadeIn(80) 
 
     playSound(randomChosenColor)
+    $("h1").text("Level "+level)
+    level += 1
 }
 
 $(".btn").click((e)=> {
@@ -18,6 +21,7 @@ $(".btn").click((e)=> {
     userClickedPattern.push(userChosenColor)
     playSound(userChosenColor)
     animatePress(userChosenColor)
+    checkAnswer(userClickedPattern.length - 1)
 })
 
 const playSound = (name) => {
@@ -27,9 +31,43 @@ const playSound = (name) => {
 
 const animatePress = (currentColor) => {
     $("#"+currentColor).addClass("pressed")
-    const i = setTimeout( () => {
+    let i = setTimeout( () => {
         $("#"+currentColor).removeClass("pressed")
     }, 100)
 }
 
-nextSequence()
+$(document).keypress(() => {
+    if(!started){
+        started = !started;
+        nextSequence()
+    }
+})
+
+const checkAnswer = (currentLevel) => {
+    if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+        if(currentLevel == gamePattern.length - 1){
+            let i = setTimeout( () => {
+                nextSequence()
+            }, 1000)
+        }
+    }
+    else {
+        let audio = new Audio(`./sounds/wrong.mp3`)
+        audio.play()
+
+        $("body").addClass("game-over")
+        
+        let i = setTimeout( () => {
+            $("body").removeClass("game-over")
+        }, 200) 
+    
+        $("h1").text("Game Over, Press Any Key to Restart")
+        startOver()
+    }
+}
+
+const startOver = () => {
+    level = 0
+    gamePattern = []
+    started = !started
+}
