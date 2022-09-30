6ad5c8a50cb82337590ed45919c1916f01ddceb1
antarpreet11 <singhantarpreet11@gmail.com>
2022-09-29 22:14:28 -0400

    all button animations using jquery

--- game.js
+++ game.js
@@ -1,6 +1,35 @@
+let gamePattern = []
+let userClickedPattern = []
+
 const buttonColors = ["red", "blue", "green", "yellow"]
 
-const newSequence = () => {
+const nextSequence = () => {
     // let randomNumber = 
-    console.log(Math.floor(Math.random() * 4));
+    let randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
+    gamePattern.push(randomChosenColor)
+
+    $("#"+randomChosenColor).fadeOut(80).fadeIn(80) 
+
+    playSound(randomChosenColor)
 }
+
+$(".btn").click((e)=> {
+    let userChosenColor = e.currentTarget.id
+    userClickedPattern.push(userChosenColor)
+    playSound(userChosenColor)
+    animatePress(userChosenColor)
+})
+
+const playSound = (name) => {
+    let audio = new Audio(`./sounds/${name}.mp3`)
+    audio.play()
+}
+
+const animatePress = (currentColor) => {
+    $("#"+currentColor).addClass("pressed")
+    const i = setTimeout( () => {
+        $("#"+currentColor).removeClass("pressed")
+    }, 100)
+}
+
+nextSequence()
